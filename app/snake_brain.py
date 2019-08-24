4cf08521a9232b24b6ec7689d9ea807bb1c8c2cc
David Hantke <hantke.david@gmail.com>
2019-08-24 16:07:44 -0600

    Cleanup debug

--- app/snake_brain.py
+++ app/snake_brain.py
@@ -27,43 +27,29 @@ class SnakeBrain(object):
         print("Board Height: {}".format(self.boardHeight))
 
     def eliminateBoardEdgeCollision(self):
-        print("*** eliminateBoardEdgeCollision")
         if self.head.x - 1 < 0:
             self.possibleMoves.remove("left")
-            print("eliminate left")
         if self.head.x + 1 == self.boardWidth:
             self.possibleMoves.remove("right")
-            print("eliminate right")
         if self.head.y - 1 < 0:
             self.possibleMoves.remove("up")
-            print("eliminate up")
         if self.head.y + 1 == self.boardHeight:
             self.possibleMoves.remove("down")
-            print("eliminate down")
 
     def eliminateSelfCollision(self):
-        print("*** eliminateSelfCollision")
         nextLeft = Coordinate(self.head.x - 1, self.head.y)
         nextRight = Coordinate(self.head.x + 1, self.head.y)
         nextUp = Coordinate(self.head.x, self.head.y - 1)
         nextDown = Coordinate(self.head.x, self.head.y + 1)
-        print("nextLeft: {}".format(str(nextLeft)))
-        print("nextRight: {}".format(str(nextRight)))
-        print("nextUp: {}".format(str(nextUp)))
-        print("nextDown: {}".format(str(nextDown)))
         for bodyPart in self.body:
             if nextLeft == bodyPart:
                 if "left" in self.possibleMoves: self.possibleMoves.remove("left")
-                print("eliminate left")
             if nextRight == bodyPart:
                 if "right" in self.possibleMoves: self.possibleMoves.remove("right")
-                print("eliminate right")
             if nextUp == bodyPart:
                 if "up" in self.possibleMoves: self.possibleMoves.remove("up")
-                print("elminate up")
             if nextDown == bodyPart:
                 if "down" in self.possibleMoves: self.possibleMoves.remove("down")
-                print("eliminate down")
 
         if not self.possibleMoves:
             for bodyPart in self.body:
